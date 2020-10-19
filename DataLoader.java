c45b435d41c92f0cf57fc7a296fd9a594c280d32
ams29 <71940967+ams29@users.noreply.github.com>
2020-10-19 14:53:55 -0500

    Add files via upload
    version 1 lmk if there needs to be changes

---
+++ DataLoader.java
@@ -0,0 +1,57 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class DataLoader {
+	
+	private HashTableMap<String, Definition> map;
+	private File file;
+	
+	public DataLoader(File file, HashTableMap<String, Definition> map)  {
+		this.map = map;
+		this.file = file;
+		try {
+			setData();
+		} catch (FileNotFoundException e) {
+			System.out.println(e.getMessage());
+		}
+	}
+	
+	
+	public void setData() throws FileNotFoundException {
+	    String KeyWord;
+	    String info;
+	    Definition addMe;
+
+	    if (file == null) {
+	      throw new FileNotFoundException("Error. No file found. Please use "
+	      		+ "valid file ending in .txt");
+	    }
+
+	    Scanner scan = new Scanner(file);
+
+	    while(scan.hasNextLine()) {
+	       KeyWord = scan.next();
+	       info = scan.nextLine();
+	       addMe = new Definition(KeyWord, info);
+
+	       map.put(KeyWord, addMe);
+	    }
+
+	    if (scan != null) {
+	    	scan.close();
+	    }
+	  }
+
+
+	public HashTableMap<String, Definition> getMap() {
+		return map;
+	}
+
+
+	public void setFile(File file) {
+		this.file = file;
+	}
+	
+	
+}
---
+++ Definition.java
@@ -0,0 +1,18 @@
+public class Definition {
+  private String KeyWord;
+  private String info;
+
+  public Definition(String KeyWord, String info) {
+       this.KeyWord = KeyWord;
+       this.info = info;
+  }
+
+  public String getKeyWord() {
+    return KeyWord;
+  }
+
+  public String get() {
+    return info;
+  }
+
+}
